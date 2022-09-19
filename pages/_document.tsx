1407c8a06575a3f1dd8f1bcdbae7f77a8ea33276
arianejay <arianejaytantan@gmail.com>
2022-09-19 09:17:09 +0800

    9/19/2022 - change tab icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,20 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
       <Head>
+        <title>ShoeMania</title>
+
+        {/* icon */}
+        <link
+          rel="../public/favicon.io"
+          href="/images/favicon.io"
+          type="image/x-icon"
+        />
+
+        {/* google fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
